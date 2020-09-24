5d7b4ab776c05175d52cc4d416a6abf580ea451e
pavel_popov <pavel.popov@concordeu.com>
2020-09-24 13:04:27 +0300

    add

--- run.bat
+++
@@ -1,5 +0,0 @@
-set ProjectPath=E:\Frameworks\AutomationRestLab
-cd %ProjectPath%\target
-java -cp Demo.jar org.testng.TestNG E:\Frameworks\AutomationRestLab\src\main\resources\xml_files\TestCase_01_UserRegistration.xml
-pause
-
